552354d1b8f3820e090d59f978033e34f0c6222b
shellyal <59883524+shellyal@users.noreply.github.com>
2022-02-23 13:19:27 -0500

    updated about images (theme folder)

--- theme/inclusivehealth_theme/front-page.php
+++ theme/inclusivehealth_theme/front-page.php
@@ -17,7 +17,7 @@
             quis nostrud. Excepteur sint occaecat cupidatat non proident, sunt
             in culpa.
           </p>
-          <img src="assets/images/together.jpg" alt="" class="bio__img">
+          <img src="<?php echo site_url(); ?>/wp-content/themes/inclusivehealth_theme/assets/images/together.jpg" alt="" class="team__img" >
 
         </div>
 
@@ -25,7 +25,7 @@
           <h1 class="section__title meettheteam__title">Meet the Team</h2>
 
           <div class="member__container">
-            <img src="assets/images/anna.png" alt="" class="team__img" />
+          <img src="<?php echo site_url(); ?>/wp-content/themes/inclusivehealth_theme/assets/images/anna.png" alt="" class="team__img" >
             <div class="info__container">
               <div class="img__background"></div>
               <h2 class="member__name">Anna Kelley</h2>
@@ -33,7 +33,7 @@
             </div>
           </div>
           <div class="member__container">
-            <img src="assets/images/becky.png" alt="" class="team__img" />
+          <img src="<?php echo site_url(); ?>/wp-content/themes/inclusivehealth_theme/assets/images/becky.png" alt="" class="team__img" >
             <div class="info__container">
               <h2 class="member__name">Becky Hutzel</h2>
               <p class="member__description">Becky Hutzel is a senior at Purdue in UX Design. She is from Lafayette, IN and is passionate about creativity, music, and people! She loves UX design because it creates a space where she can blend these three areas. She started off in Industrial Design and gained skills from this in sketching, Adobe suite, Rhinoceros, and Catia. She did interface and web design over summer 2019 for a research project called the Biowall. Her work for the Biowall included making an interface completely from scratch to display intricate computer science data in terms everyday homeowners would understand. During summer 2021 she had an interface design internship where she tweaked the current layout of a company’s application. When she is not doing interface or graphic design work you can find her doing a variety of outdoor activities with the Purdue Outing Club which she is the Vice President of. You can contact her at hutzelr6@purdue.edu.</p> 
@@ -65,7 +65,7 @@
           </div>
 
           <div class="member__container">
-            <img src="/assets/images/victoria.png" alt="" class="team__img" />
+          <img src="<?php echo site_url(); ?>/wp-content/themes/inclusivehealth_theme/assets/images/victoria.png" alt="" class="team__img" >
             <div class="info__container">
               <h2 class="member__name">Victoria Gromek</h2>
               <p class="member__description">Victoria Gromek is a senior at Purdue University majoring in Web Programming & Design. She’s also pursuing a major in Psychological Sciences. She is passionate about creativity, music, cats, and people. Her family encourages her the most to be the best she can be. Her focus shifted more to wanting to help others when she decided to add another major that meant a lot to her growing up. She is actively working to utilize both majors into something she is proud of that will benefit others. Her hobbies include painting, cooking, and helping out her sisters of Sigma Delta Tau. You can always find her with any one of her sisters that come from a variety of backgrounds and majors learning more about herself and the world around her. Victoria is also the current secretary of her chapter serving the term from Spring 2021 to Fall 2021. She is always willing to help others and do what she can, even if it's as simple as taking a walk with someone. Feel free to contact Victoria at vgromek@purdue.edu.</p>
@@ -73,7 +73,7 @@
           </div>
 
           <div class="member__container">
-            <img src="assets/images/hutzel.png" alt="" class="team__img" />
+          <img src="<?php echo site_url(); ?>/wp-content/themes/inclusivehealth_theme/assets/images/hutzel.png" alt="" class="team__img" >
             <div class="info__container">
               <h2 class="member__name">Yuhan Wu</h2>
               <p class="member__description">Yuhan Wu is a junior student at Purdue University majoring in Data Visualization and Information Technology. Her minor in management aids her organization and management ability. She grew up in China and her diverse cultural backgrounds developed her creativity and inclusiveness. She focused mainly on data analysis and machine learning, intended to combine visualized data with emerging industries of technology. You can contact her at wu1352@purdue.edu</p>
